52fdf793d47efa320120bfcacc28adc8001ff43b
Hardy <1277146050@qq.com>
2020-06-18 17:03:48 +0800

    add files

---
+++ multiplication_table/number/index.js
@@ -0,0 +1,10 @@
+for(var i = 1; i <= 9; i++){
+  for(var j = 1 ;  j <= i; j++) {
+      document.write(i + '*'+ j + '=' + (i*j )+ ' ' )
+  }
+  document.write('<br>');
+}
+
+
+var a = 1;
+console.log(`a的值是：${a}`)
---
+++ number/index.js
@@ -0,0 +1,7 @@
+for(var i = 1; i <= 20; i++){
+    if(i % 2 == 0) {
+        document.writeln(i + " is even number. <br>"  )
+    } else {
+        document.writeln(i + " is odd number. <br>")
+    }
+}
